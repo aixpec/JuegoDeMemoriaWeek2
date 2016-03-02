c10eaf4e1c15ee2cc08d02492a4c5229bcd74c6f
aixpec <aixpec@gisystemsint.com>
2016-03-02 00:03:41 -0600

    Initial Commit

---
+++ Juego de Memoria Week 2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Juego de Memoria Week 2
+//
+//  Created by Andrés Ixpec on 2/03/16.
+//  Copyright © 2016 Andrés Ixpec. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
